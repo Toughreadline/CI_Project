44c9848ab2cf79578fb2c70e66e698f678240240
temi-daniel <adeoyetemiloluwa54@gmail.com>
2026-05-30 10:29:34 -0700

    app.py update

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
+
+
 @app.route('/')
 def home():
- return render_template('index.html', page='home')
+    return render_template('index.html', page='home')
+
+
 @app.route('/health')
 def health():
- return {'status': 'ok'}, 200
+    return {'status': 'ok'}, 200
+
+
 if __name__ == '__main__':
- app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=5000, debug=False)
