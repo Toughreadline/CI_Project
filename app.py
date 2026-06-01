a4d79a66e948a2b91c94d8ca38bb93d1eb0300d6
temi-daniel <adeoyetemiloluwa54@gmail.com>
2026-05-31 20:55:04 -0700

    About page

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 def home():
     return render_template('index.html', page='home')
 
+@app.route('/about')
+def about():
+    return render_template('about.html', page='about')
 
 @app.route('/health')
 def health():
